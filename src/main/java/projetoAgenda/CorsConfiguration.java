a48a94381b6dc915f233ffe46dc72cfbb41848df
renanpacheco21 <renan1025>
2022-06-30 20:26:19 -0300

    RenanCommit

--- src/main/java/projetoAgenda/CorsConfiguration.java
+++ src/main/java/projetoAgenda/CorsConfiguration.java
@@ -11,7 +11,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         //liberando app cliente 1
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://localhost:8000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
 
     }
