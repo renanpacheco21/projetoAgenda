f3d5aa49417b303800ba745a2ef7ea6c70d57624
renanpacheco21 <renan1025>
2022-06-30 17:46:06 -0300

    RenanCommit

--- src/main/java/projetoAgenda/resource/ProcessoController.java
+++ src/main/java/projetoAgenda/resource/ProcessoController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import projetoAgenda.model.Processo;
 import projetoAgenda.repository.ProcessoRepository;
 
 import javax.persistence.EntityNotFoundException;
@@ -17,44 +18,44 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/responsavel")
+@RequestMapping("/api/processo")
 public class ProcessoController {
     @Autowired
     private ProcessoRepository repository;
 
     @GetMapping
-    public List<ResponsavelDTO> getResponsavel(@QuerydslPredicate(root = Responsavel.class) Predicate predicate){
-        List<ResponsavelDTO> result = new ArrayList<>();
-        Iterable<Responsavel> all = repository.findAll(predicate);
-        all.forEach(f -> result.add(ResponsavelDTO.toDTO(f)));
+    public List<ProcessoDTO> getProcesso(@QuerydslPredicate(root = Processo.class) Predicate predicate){
+        List<ProcessoDTO> result = new ArrayList<>();
+        Iterable<Processo> all = repository.findAll(predicate);
+        all.forEach(f -> result.add(ProcessoDTO.toDTO(f)));
         return result;
     }
 
     @GetMapping("/{id}")
-    public ResponsavelDTO getResponsavelsId(@PathVariable(value = "id") Long responsavelId) throws EntityNotFoundException {
-        Responsavel responsavelFind = repository.findById(responsavelId).orElseThrow(() -> new EntityNotFoundException("Responsável não encontado com o ID" +responsavelId));
-        return ResponsavelDTO.toDTO(responsavelFind);
+    public ProcessoDTO getProcessoId(@PathVariable(value = "id") Long processoId) throws EntityNotFoundException {
+        Processo processoFind = repository.findById(processoId).orElseThrow(() -> new EntityNotFoundException("Processo não encontado com o ID" + processoId));
+        return ProcessoDTO.toDTO(processoFind);
     }
 
     @PostMapping
-    public ResponsavelDTO create(@Valid @RequestBody Responsavel responsavel){
-        return ResponsavelDTO.toDTO(repository.save(responsavel));
+    public ProcessoDTO create(@Valid @RequestBody Processo processo){
+        return ProcessoDTO.toDTO(repository.save(processo));
     }
 
-    @PutMapping("/{id}")
-    public ResponsavelDTO update(@PathVariable(value = "id") Long responsavelId,
-                                 @RequestBody Responsavel responsavel) throws EntityNotFoundException {
-        Responsavel responsavelFind = repository.findById(responsavelId).orElseThrow(() -> new EntityNotFoundException("Responsável não encontrado com ID :: " +responsavelId));
-        responsavelFind.setId(responsavel.getId());
-        responsavelFind.setNome(responsavel.getNome());
-        responsavelFind.setPopulacao(responsavel.getPopulacao());
-        return ResponsavelDTO.toDTO(repository.save(responsavelFind));
-    }
+//    @PutMapping("/{id}")
+//    public ProcessoDTO update(@PathVariable(value = "id") Long responsavelId,
+//                                 @RequestBody Processo responsavel) throws EntityNotFoundException {
+//        Processo responsavelFind = repository.findById(responsavelId).orElseThrow(() -> new EntityNotFoundException("Responsável não encontrado com ID :: " +responsavelId));
+//        responsavelFind.setId(responsavel.getId());
+//        responsavelFind.setNome(responsavel.getNome());
+//        responsavelFind.setPopulacao(responsavel.getPopulacao());
+//        return ProcessoDTO.toDTO(repository.save(responsavelFind));
+//    }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity delete(@PathVariable(value = "id") Long responsavelId) throws EntityNotFoundException{
-        Responsavel responsavelFind = repository.findById(responsavelId).orElseThrow(() -> new EntityNotFoundException("Responsável não encontrado com o ID::" +responsavelId));
-        repository.delete(responsavelFind);
+    public ResponseEntity delete(@PathVariable(value = "id") Long processoId) throws EntityNotFoundException{
+        Processo processoFind = repository.findById(processoId).orElseThrow(() -> new EntityNotFoundException("Processo não encontrado com o ID::" + processoId));
+        repository.delete(processoFind);
         return ResponseEntity.noContent().build();
     }
 
